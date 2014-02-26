6d30c36fa5d6679d91a56742cbcb9c0388248590
ThanhNguyen <nguyenvanthanh9294@gmail.com>
2014-02-27 01:21:44 +0700

    add folder test C

---
+++ Bai1/bai1.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+
+int dequychinhphuong(n)
+{
+	for(int i=0;i<n/2;i++)
+	{
+		return i*i;
+	}
+}
+
+
+int main()
+{
+	for(int i=0;i<n/2;i++)
+	{
+		cout<<"dequychinhphuong(2000);
+	}
+}
+
